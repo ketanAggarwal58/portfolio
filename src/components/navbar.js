4ab96ddfe2ea8fb65091a583742e43db7f9e2742
Ketan Aggarwal <64477717+ketanAggarwal58@users.noreply.github.com>
2022-07-14 18:52:51 +0530

    navbar bugs fixed

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -10,19 +10,19 @@ function Navbar(props){
                         <span className='logo'>KAggarwal</span>
                     </NavLink>
                     <div className='Navbar-items'>
-                        <NavLink className="nav-link" to="/projects">
+                        <NavLink className="{({ isActive }) => (isActive ? 'active' : 'inactive')}" to="/projects">
                             <span>Projects</span>
                         </NavLink>
-                        <NavLink className="nav-link" to="/blogs">
+                        <NavLink className={({ isActive }) => (isActive ? 'active' : 'inactive')} to="/blogs">
                             <span>Blogs</span>
                         </NavLink>
-                        <NavLink className="nav-link" to="/podcasts">
+                        <NavLink className={({ isActive }) => (isActive ? 'active' : 'inactive')} to="/podcasts">
                             <span>Podcasts</span>
                         </NavLink>
-                        <NavLink className="nav-link" to="/skills">
+                        <NavLink className={({ isActive }) => (isActive ? 'active' : 'inactive')} to="/skills">
                             <span>Skills</span>
                         </NavLink>
-                        <NavLink className="nav-link" to="/about">
+                        <NavLink className={({ isActive }) => (isActive ? 'active' : 'inactive')} to="/about">
                             <span>About</span>
                         </NavLink>
                     </div>
