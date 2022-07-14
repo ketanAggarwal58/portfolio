69c36f0dc2d26dcd1835e05d018832c2c1671b88
Ketan Aggarwal <64477717+ketanAggarwal58@users.noreply.github.com>
2022-07-14 18:53:02 +0530

    home bar issue fixed

--- src/screens/home.js
+++ src/screens/home.js
@@ -1,18 +1,13 @@
 import React from "react";
 import '../style/home.css'
-import Navbar from'../components/navbar'
 
 function Home(){
     return(
         <>
-            <Navbar />
-            <div className='content-container'>
-              <div className='image-container'></div>
-              <div className='Info-container'>
-                <div className='data-container-1'></div>
-                <div className='data-container-2'></div>
-              </div>
-            </div>
+          <div className='content-container'>
+            <span>Hi, Welcome to the Official Portfolio of</span>
+            <span>The Ketan Aggarwal!</span>
+          </div>
         </>
     );
 }
