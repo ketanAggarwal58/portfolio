93d252331789c1139a4004e0088ab4520965fa31
Ketan Aggarwal <64477717+ketanAggarwal58@users.noreply.github.com>
2022-07-14 12:05:26 +0530

    minor bug fixed

--- src/routes/route.js
+++ src/routes/route.js
@@ -5,20 +5,24 @@ import {
   } from "react-router-dom";
 import Home from "../screens/home";
 import About from "../screens/about";
+import Projects from "../screens/projects";
+import Skills from "../screens/skills"
+import Navbar from "../components/navbar";
+import Podcasts from "../screens/podcasts";
+import Blog from "../screens/blog";
 
 export const Routers = (props) => {
     return (
         <main>
             <Router>
-                <Home />
+                <Navbar />
                 <Routes>
                     <Route exact path="/" element={<Home />} />
-                    <Route exact path="/home" element={<Home />} />
                     <Route exact path="/about" element={<About />} />
-                    <Route exact path="/project" element={<About />} />
-                    <Route exact path="/skills" element={<About />} />
-                    <Route exact path="/podcats" element={<About />} />
-                    <Route exact path="/blogs" element={<About />} />
+                    <Route exact path="/project" element={<Projects />} />
+                    <Route exact path="/skills" element={<Skills />} />
+                    <Route exact path="/podcats" element={<Podcasts />} />
+                    <Route exact path="/blogs" element={<Blog />} />
                 </Routes>
             </Router>
         </main>
